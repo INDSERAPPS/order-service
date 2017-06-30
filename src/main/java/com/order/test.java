28b276b3e64783c3e4225d8f39303d40ced5788d
COF\RXD117 <rxd117@login.capone.com>
2017-06-29 20:47:58 -0400

    orders code

---
+++ src/main/java/com/order/test.java
@@ -0,0 +1,34 @@
+package com.order;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class test {
+
+	@Id
+	@Column(name = "testid")
+	private int testid;
+
+	@Id
+	@Column(name = "name")
+	private String name;
+
+	public int getTestId() {
+		return testid;
+	}
+
+	public void setTestId(int testid) {
+		this.testid = testid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
