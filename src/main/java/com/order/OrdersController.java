fdb86b99f06116c4e39277dd72231d4298b0c1d4
COF\RXD117 <rxd117@login.capone.com>
2017-07-06 01:13:35 -0400

    added eureka client excluded tomcat

--- src/main/java/com/order/OrdersController.java
+++ src/main/java/com/order/OrdersController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(path = "/order")
+@RequestMapping(path = "/orders")
 public class OrdersController {
 
 	@Autowired // This means to get the bean called userRepository
